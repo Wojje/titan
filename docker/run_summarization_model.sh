6a42741ca49919bc9a12fae6ca23a3eea1c394b0
Joppe Widstam <joppewidstam@hotmail.com>
2016-03-22 21:13:16 +0100

    Fixed paths to start_scripts

--- docker/run_summarization_model.sh
+++ docker/run_summarization_model.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/joppew/titan/docker/docker_run_gpu.sh --rm -t -v /home/joppew/logs_2_layers:/logs_2_layers joppew/asudl:latest bash model_bbc/start_script.sh
+/home/joppew/titan/docker/docker_run_gpu.sh --rm -t -v /home/joppew/logs_2_layers:/logs_2_layers joppew/asudl:latest bash /model_bbc/start_script.sh
