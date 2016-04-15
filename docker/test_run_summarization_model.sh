e41ffa3e4306797a76c50681a1f7665543437f67
Joppe Widstam <joppewidstam@hotmail.com>
2016-04-15 22:47:26 +0200

    logs path to logs_dropout and script path to model/script

--- docker/test_run_summarization_model.sh
+++ docker/test_run_summarization_model.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/joppew/titan/docker/docker_run_gpu.sh --rm -t -v /home/joppew/test_logs:/test_logs joppew/asudl:latest bash model_bbc/test_start_script.sh
+/home/joppew/titan/docker/docker_run_gpu.sh --rm -t -v /home/joppew/test_logs:/test_logs joppew/asudl:latest bash /model/test_start_script.sh
