883685487415f8c17092ee0b43900c51c6acdec0
Joppe Widstam <joppewidstam@hotmail.com>
2016-04-16 11:41:21 +0200

    Changed to latest-dropout

--- docker/run_summarization_model.sh
+++ docker/run_summarization_model.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/joppew/titan/docker/docker_run_gpu.sh --rm -t -v /home/joppew/logs_dropout:/logs_dropout joppew/asudl:latest bash /model/start_script.sh
+/home/joppew/titan/docker/docker_run_gpu.sh --rm -t -v /home/joppew/logs_dropout:/logs_dropout joppew/asudl:latest-dropout bash /model/start_script.sh
