89642fe1bef99e83ed00bd4dbdccee4021e39c16
Joppe Widstam <joppewidstam@hotmail.com>
2016-05-17 22:12:21 +0200

    Using only one gpu and trying 1 layer with size 1000.

--- docker/run_summarization_model.sh
+++ docker/run_summarization_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #CUDA_VISIBLE_DEVICES=0 /home/joppew/titan/docker/docker_run_gpu.sh --name="${USER}0" --rm -t -v /home/joppew/logs_no_dropout_500_2:/logs joppew/asudl:latest bash /model/start_script.sh --data_dir=/data/ --logs_dir=/logs/ --train_dir=/logs/ --size=1 --num_layers=1 --batch_size=64 --vocab_size=30000 --steps_per_checkpoint=100 --steps_per_scheduled_job=500 &
 #FIRSTPID=${!};
-CUDA_VISIBLE_DEVICES=1 /home/joppew/titan/docker/docker_run_gpu.sh --name="${USER}1" --rm -t -v /home/joppew/logs_dropout_1000_1:/logs joppew/asudl:latest bash /model/start_script.sh --data_dir=/data/ --logs_dir=/logs/ --train_dir=/logs/ --size=1 --num_layers=1 --batch_size=64 --dropout_keep_prob=0.5 --vocab_size=30000 --steps_per_checkpoint=100 --steps_per_scheduled_job=500 &
+CUDA_VISIBLE_DEVICES=1 /home/joppew/titan/docker/docker_run_gpu.sh --name="${USER}1" --rm -t -v /home/joppew/logs_dropout_1000_1:/logs joppew/asudl:latest bash /model/start_script.sh --data_dir=/data/ --logs_dir=/logs/ --train_dir=/logs/ --size=1000 --num_layers=1 --batch_size=64 --dropout_keep_prob=0.5 --vocab_size=30000 --steps_per_checkpoint=1000 --steps_per_scheduled_job=1000 &
 SECONDPID=${!};
 #CUDA_VISIBLE_DEVICES=2 /home/joppew/titan/docker/docker_run_gpu.sh --name="${USER}2" --rm -t -v /home/joppew/logs_no_dropout_1000_1:/logs joppew/asudl:latest bash /model/start_script.sh --data_dir=/data/ --logs_dir=/logs/ --train_dir=/logs/ --size=1 --num_layers=1 --batch_size=64 --vocab_size=30000 --steps_per_checkpoint=100 --steps_per_scheduled_job=500 &
 #THIRDPID=${!};
@@ -10,5 +10,5 @@ SECONDPID=${!};
 #wait $FIRSTPID 
 wait $SECONDPID #$THIRDPID $FOURTHPID
 
-# bash /home/joppe/titan/run_summarization_model.sh &
+bash /home/joppe/titan/run_summarization_model.sh &
 
