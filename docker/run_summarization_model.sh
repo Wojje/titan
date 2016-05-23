21c31f72702d7d014e3cf010d7238c7fac0efb1b
Joppe Widstam <joppewidstam@hotmail.com>
2016-05-23 20:52:14 +0200

    Trying using two processes (dropout_256_2 and no_dropout_512_1) again on cuda device 0 and 2.

--- docker/run_summarization_model.sh
+++ docker/run_summarization_model.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=0 /home/joppew/titan/docker/docker_run_gpu.sh --name="${USER}0" --rm -t -v /home/joppew/logs_dropout_256_2:/logs joppew/asudl:latest bash /model/start_script.sh --data_dir=/data/ --logs_dir=/logs/ --train_dir=/logs/ --size=256 --num_layers=2 --batch_size=64 --dropout_keep_prob=0.5 --vocab_size=30000 --steps_per_checkpoint=100 --steps_per_scheduled_job=500 &
 FIRSTPID=${!};
-#CUDA_VISIBLE_DEVICES=1 /home/joppew/titan/docker/docker_run_gpu.sh --name="${USER}1" --rm -t -v /home/joppew/logs_no_dropout_512_1:/logs joppew/asudl:latest bash /model/start_script.sh --data_dir=/data/ --logs_dir=/logs/ --train_dir=/logs/ --size=512 --num_layers=1 --batch_size=64 --vocab_size=30000 --steps_per_checkpoint=100 --steps_per_scheduled_job=500 &
-#SECONDPID=${!};
+CUDA_VISIBLE_DEVICES=2 /home/joppew/titan/docker/docker_run_gpu.sh --name="${USER}2" --rm -t -v /home/joppew/logs_no_dropout_512_1:/logs joppew/asudl:latest bash /model/start_script.sh --data_dir=/data/ --logs_dir=/logs/ --train_dir=/logs/ --size=512 --num_layers=1 --batch_size=64 --vocab_size=30000 --steps_per_checkpoint=100 --steps_per_scheduled_job=500 &
+SECONDPID=${!};
 
-wait $FIRSTPID #$SECONDPID
+wait $FIRSTPID $SECONDPID
 
 #CUDA_VISIBLE_DEVICES=0 /home/joppew/titan/docker/docker_run_gpu.sh --name="${USER}0" --rm -t -v /home/joppew/logs_no_dropout_500_2:/logs joppew/asudl:latest bash /model/start_script.sh --data_dir=/data/ --logs_dir=/logs/ --train_dir=/logs/ --size=1 --num_layers=1 --batch_size=64 --vocab_size=30000 --steps_per_checkpoint=100 --steps_per_scheduled_job=500 &
 #FIRSTPID=${!};
@@ -15,6 +15,3 @@ wait $FIRSTPID #$SECONDPID
 #CUDA_VISIBLE_DEVICES=3 /home/joppew/titan/docker/docker_run_gpu.sh --name="${USER}3" --rm -t -v /home/joppew/logs_dropout_500_2:/logs joppew/asudl:latest bash /model/start_script.sh --data_dir=/data/ --logs_dir=/logs/ --train_dir=/logs/ --vocab_size=30000 --dropout_keep_prob=0.5 --size=1 --num_layers=1 --batch_size=64 --steps_per_checkpoint=100 --steps_per_scheduled_job=500 &
 #FOURTHPID=${!};
 #wait $FIRSTPID $SECONDPID #$THIRDPID $FOURTHPID
-
-#bash /home/joppe/titan/run_summarization_model.sh &
-
