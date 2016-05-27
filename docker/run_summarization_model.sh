d4d26baafb64ff0ac6f0cfc1837446b12949883d
Joppe Widstam <joppewidstam@hotmail.com>
2016-05-27 12:28:25 +0200

    Trying one process (dropout_256_2) with input length 750.

--- docker/run_summarization_model.sh
+++ docker/run_summarization_model.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=1 /home/joppew/titan/docker/docker_run_gpu.sh --name="${USER}1" --rm -t -v /home/joppew/logs_dropout_256_2:/logs joppew/asudl:latest bash /model/start_script.sh --data_dir=/data/ --logs_dir=/logs/ --train_dir=/logs/ --size=256 --num_layers=2 --batch_size=64 --dropout_keep_prob=0.5 --vocab_size=30000 --steps_per_checkpoint=100 --steps_per_scheduled_job=500 &
-FIRSTPID=${!};
-CUDA_VISIBLE_DEVICES=2 /home/joppew/titan/docker/docker_run_gpu.sh --name="${USER}2" --rm -t -v /home/joppew/logs_no_dropout_512_1:/logs joppew/asudl:latest bash /model/start_script.sh --data_dir=/data/ --logs_dir=/logs/ --train_dir=/logs/ --size=512 --num_layers=1 --batch_size=64 --vocab_size=30000 --steps_per_checkpoint=100 --steps_per_scheduled_job=500 &
-SECONDPID=${!};
+#FIRSTPID=${!};
+#CUDA_VISIBLE_DEVICES=2 /home/joppew/titan/docker/docker_run_gpu.sh --name="${USER}2" --rm -t -v /home/joppew/logs_no_dropout_512_1:/logs joppew/asudl:latest bash /model/start_script.sh --data_dir=/data/ --logs_dir=/logs/ --train_dir=/logs/ --size=512 --num_layers=1 --batch_size=64 --vocab_size=30000 --steps_per_checkpoint=100 --steps_per_scheduled_job=500 &
+#SECONDPID=${!};
 
-wait $FIRSTPID $SECONDPID
+#wait $FIRSTPID $SECONDPID
 
 #CUDA_VISIBLE_DEVICES=0 /home/joppew/titan/docker/docker_run_gpu.sh --name="${USER}0" --rm -t -v /home/joppew/logs_no_dropout_500_2:/logs joppew/asudl:latest bash /model/start_script.sh --data_dir=/data/ --logs_dir=/logs/ --train_dir=/logs/ --size=1 --num_layers=1 --batch_size=64 --vocab_size=30000 --steps_per_checkpoint=100 --steps_per_scheduled_job=500 &
 #FIRSTPID=${!};
