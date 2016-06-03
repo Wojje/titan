aa70d0520d1259388a4dca624f5f48ed20dcb7bf
Joppe Widstam <joppewidstam@hotmail.com>
2016-06-03 13:36:38 +0200

    added gpu selection to job submit script.

--- run_summarization_model.sh
+++ run_summarization_model.sh
@@ -7,7 +7,8 @@ project_run=$(basename "$0")
 mkdir log 
 
 #submit job
-qsub -cwd -l gpu=2 \
+qsub -cwd \
+  -l gpu=1 \
   -e ./log/$project_run.error \
   -o ./log/$project_run.log \
   ./docker/$project_run
