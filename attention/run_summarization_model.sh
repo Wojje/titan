6ebdd8fd729eadbe3f733d56a63a161b36d43c73
Joppe Widstam <joppewidstam@hotmail.com>
2016-06-08 16:37:10 +0200

    Fixed paths.

--- attention/run_summarization_model.sh
+++ attention/run_summarization_model.sh
@@ -9,7 +9,7 @@ mkdir log
 #submit job
 qsub -cwd \
   -l gpu=1 \
-  -e ./log/$project_run.error \
-  -o ./log/$project_run.log \
-  ./docker/$project_run
+  -e /home/joppew/titan/attention/log/$project_run.error \
+  -o /home/joppew/titan/attention/log/$project_run.log \
+  /home/joppew/titan/attention/docker/$project_run
 
--- no_attention/run_summarization_model.sh
+++ no_attention/run_summarization_model.sh
@@ -9,7 +9,7 @@ mkdir log
 #submit job
 qsub -cwd \
   -l gpu=1 \
-  -e ./log/$project_run.error \
-  -o ./log/$project_run.log \
-  ./docker/$project_run
+  -e /home/joppew/titan/no_attention/log/$project_run.error \
+  -o /home/joppew/titan/no_attention/log/$project_run.log \
+  /home/joppew/titan/no_attention/docker/$project_run
 
